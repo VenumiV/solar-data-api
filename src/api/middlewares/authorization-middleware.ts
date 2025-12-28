255f8313d38d6c6ed9c62bbbd62fa083174bddd0
Venumi Alagoda <166162600+VenumiV@users.noreply.github.com>
2025-12-29 00:00:22 +0530

    Rename project from "fs_back_end" to "fs_data_api" in package.json and package-lock.json; refactor authorization middleware to simplify user authentication checks.

--- src/api/middlewares/authorization-middleware.ts
+++ src/api/middlewares/authorization-middleware.ts
@@ -1,21 +1,17 @@
-import { getAuth } from "@clerk/express"
-import { ForbiddenError, UnauthorizedError } from "../../domain/errors/error";
 import { NextFunction, Request, Response } from "express";
-import { User } from "../../infrastructure/entities/User";
-import {UserPublicMetadata } from "../../domain/types";
-
-
+import { getAuth } from "@clerk/express";
+//import { User } from "../../infrastructure/entities/User";
+import { ForbiddenError, UnauthorizedError } from "../../domain/errors/error";
+import { UserPublicMetadata } from "../../domain/types";
 
 export const authorizationMiddleware = async (
     req: Request,
     res: Response,
     next: NextFunction
-
 ) => {
     const auth = getAuth(req);
-    const user = await User.findOne({ clerkUserId: auth.userId});
-    if (!user){
-        throw new UnauthorizedError("Unauthorized")
+    if (!auth.userId) {
+        throw new UnauthorizedError("Unauthorized");
     }
 
     const publicMetadata = auth.sessionClaims?.metadata as UserPublicMetadata;
@@ -23,6 +19,5 @@ export const authorizationMiddleware = async (
     if (publicMetadata.role !== "admin") {
         throw new ForbiddenError("Forbidden");
     }
-    
     next();
-}
+};
